fb6e45aa8bfba5af9d0fd0a61b989fdd3ff497c3
Josh <joshuasevendavid@gmail.com>
2022-12-17 15:14:49 -0500

    add cards together, display in Sum

--- index.js
+++ index.js
@@ -1,12 +1,45 @@
-var firstCard = Math.floor(Math.random() * 11) + 2;
-var secondCard = Math.floor(Math.random() * 11) + 2;
+// Check player is 21 or older
+let age = 22;
 
-var sum = firstCard + secondCard;
-
-if(sum < 21){
-    console.log("You have " + sum + "; draw another card? 🙂");
-}else if(sum === 21){
-    console.log("Black Jack! You win! 🥳")
-}else{
-    console.log("You busted with " + sum + "! 😭");
+if (age < 21) {
+  console.log("Sorry, you're too young to play.");
+} else {
+  console.log("Have a seat at the Black Jack table!");
 }
+// Get cards and play Black Jack
+var firstCard = document.getElementById("firstCard")
+var secondCard = document.getElementById("secondCard")
+var sum = firstCard + secondCard;
+let sumEl = document.querySelector("#sum-el")
+
+console.log(sumEl)
+
+let hasBlackJack = false;
+let isAlive = true;
+
+let message = "";
+let messageEl = document.getElementById("message-el")
+
+function startGame() {
+    firstCard = Math.floor(Math.random() * 11) + 2;
+    secondCard = Math.floor(Math.random() * 11) + 2;
+    var sum = firstCard + secondCard;
+    firstCard.textContent = firstCard
+    secondCard.textContent = secondCard
+    sumEl.textContent = "Sum: " + sum
+  if (sum < 21) {
+    message = "You have " + sum + "; draw another card?";
+  } else if (sum === 21) {
+    message = "21 is Black Jack! You win!";
+    hasBlackJack = true;
+  } else {
+    message = "You busted with " + sum + "!";
+    isAlive = false;
+  }
+  // Log out message
+  messageEl.textContent = message
+}
+
+// CASH OUT: create a variable to keep track of state of game
+console.log(hasBlackJack);
+console.log(isAlive);
