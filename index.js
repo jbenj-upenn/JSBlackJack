af9a22236668d8c78b1ed09381efcfed82e61137
Josh <joshuasevendavid@gmail.com>
2022-12-20 15:40:24 -0500

    working to add new card to total if player chooses to receive another card after first two

--- index.js
+++ index.js
@@ -12,23 +12,39 @@ var secondCardEl = document.getElementById("secondCard")
 // var sum = firstCard + secondCard;
 let sumEl = document.querySelector("#sum-el")
 
+
+
+let cards = [firstCardEl, secondCardEl] //array: ordered list of items
+let card = 7;
+
+var sum = cards[0] + cards[1];
+
 let hasBlackJack = false;
 let isAlive = true;
 
 let message = "";
 let messageEl = document.getElementById("message-el")
 
+let newCardEl = document.querySelector("#new-card-el");
+
 function startGame() {
-    firstCard = Math.floor(Math.random() * 11) + 2;
-    secondCard = Math.floor(Math.random() * 11) + 2;
-    var sum = firstCard + secondCard;
-    firstCard.textContent = firstCard
-    secondCard.textContent = secondCard
-    firstCardEl.textContent = "First Card: " + firstCard
-    secondCardEl.textContent = "; Second Card: " + secondCard
+    renderGame()
+}
+
+function renderGame() {
+    // render first two cards
+    cards[0] = Math.floor(Math.random() * 11) + 2;
+    cards[1] = Math.floor(Math.random() * 11) + 2;
+
+    firstCardEl.textContent = "First Card: " + cards[0]
+    secondCardEl.textContent = "; Second Card: " + cards[1]
+    
+    //render all cards we have
+    var sum = cards[0] + cards[1];
     sumEl.textContent = "You have: " + sum
+
   if (sum < 21) {
-    message = "You have " + sum + "; draw another card?";
+    message = "Draw another card with " + sum + "?";
   } else if (sum === 21) {
     message = "21 is Black Jack! You win!";
     hasBlackJack = true;
@@ -36,9 +52,25 @@ function startGame() {
     message = "You busted with " + sum + "!";
     isAlive = false;
   }
+
   // Log out message
   messageEl.textContent = message
 }
+
+function newCard(){
+    console.log("new card drawn");
+    newCardEl.textContent = "Hit? "
+    card = Math.floor(Math.random() * 11) + 2;
+    
+    sum += card;
+    cards.push(card)
+
+    renderGame()
+    
+    console.log(cards)
+    
+
+  }
 
 // CASH OUT: create a variable to keep track of state of game
 console.log(hasBlackJack);
